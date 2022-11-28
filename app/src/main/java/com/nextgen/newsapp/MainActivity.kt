061353814aeff4a34d89f1616349b5868393859e
ribakmasude <beranjusihombing1@gmail.com>
2022-11-28 15:01:37 +0700

    edit home

--- app/src/main/java/com/nextgen/newsapp/MainActivity.kt
+++ app/src/main/java/com/nextgen/newsapp/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.nextgen.newsapp
 
 import android.os.Bundle
+import android.view.View
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.navigation.NavDestination
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
@@ -14,7 +17,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
-
+        installSplashScreen()
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -25,9 +28,35 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(setOf(
-            R.id.navigation_home, R.id.navigation_news, R.id.navigation_profile))
-        setupActionBarWithNavController(navController, appBarConfiguration)
+//        val appBarConfiguration = AppBarConfiguration(setOf(
+//            R.id.navigation_home, R.id.navigation_news, R.id.navigation_profile))
+//        setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+        navController.addOnDestinationChangedListener { _, destionation: NavDestination, _ ->
+            when(destionation.id){
+                R.id.navigation_home -> {
+                    visible()
+                }
+                R.id.navigation_news -> {
+                    visible()
+                }
+                R.id.navigation_profile -> {
+                    visible()
+                }
+                else -> {
+                    inVisible()
+                }
+            }
+        }
     }
+
+    private fun inVisible() {
+        binding.navView.visibility = View.GONE
+    }
+
+    private fun visible() {
+        binding.navView.visibility = View.VISIBLE
+    }
+
+
 }
